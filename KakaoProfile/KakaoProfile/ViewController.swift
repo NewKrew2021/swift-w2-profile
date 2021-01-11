52a572616f5f44484d1ee3d15a0b789b625d486e
blue.1 <blue.1@kakaocorp.com>
2021-01-11 19:53:31 +0900

    Step1 - 프로젝트 생성, Tab Bar Controller 추가

---
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  KakaoProfile
+//
+//  Created by 이청원 on 2021/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
