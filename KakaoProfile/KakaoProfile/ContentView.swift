a156b2a15956768de2b4f5aa711c91ef38f25f89
Herb Salt <herb.salt@kakaocorp.com>
2021-01-11 17:25:07 +0900

    make project

---
+++ KakaoProfile/KakaoProfile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KakaoProfile
+//
+//  Created by herb.salt on 2021/01/11.
+//  Copyright © 2021 com.kakaocorp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
