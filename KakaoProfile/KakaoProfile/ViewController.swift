d5d067216a1531dac26246146697b64688460360
Herb Salt <herb.salt@kakaocorp.com>
2021-01-11 19:13:47 +0900

    finish first step

---
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  KakaoProfile
+//
+//  Created by herb.salt on 2021/01/11.
+//  Copyright © 2021 com.kakaocorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+    }
+
+
+}
+
