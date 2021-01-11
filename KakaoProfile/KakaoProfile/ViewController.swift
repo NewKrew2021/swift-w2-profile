ef7a1dc013ef81306b323a9d3d21bd1a2f34d744
eden.joon <eden.joon@kakaocorp.com>
2021-01-11 18:59:15 +0900

    컴포넌트 연결 및 리스너 설정
    버튼과 라벨들을 추가하고 컨트롤러 파일에 연결해 값 접근, 이벤트 콜백을 구현

--- KakaoProfile/KakaoProfile/ViewController.swift
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(#file, #line, #function, #column)
+        
+        self.nameLabel.text = "Eden"
+        self.nameLabel.backgroundColor = UIColor.blue
+        self.nameLabel.textColor = UIColor.gray
+        self.nameLabel.alpha = 50
+        self.nameLabel.font = UIFont(name: "System", size: 100)
+        self.descriptionLabel.text = "팬 소음이 너무 크다"
+        
+        
     }
-
-
+    
+    @IBAction func editButtonTouched(_ sender: Any) {
+        self.nameLabel.textColor = UIColor.blue
+        self.nameLabel.backgroundColor = UIColor.yellow
+        self.nameLabel.alpha = 0.5
+        self.descriptionLabel.text = "크루미션"
+    }
 }
 
