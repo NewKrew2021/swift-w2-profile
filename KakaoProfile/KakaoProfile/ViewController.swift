d0590e664031d6e302caa3f7dc66da6fc17b3167
David Window <david.window@kakaocorp.com>
2021-01-12 12:43:27 +0900

    IBOutlet 프로그래밍 요구사항 충족
    Main.storyboard의 item1 ViewController에 ImageView, Label, Button 추가
    InmageView, Label, Button을 ViewController 연결
    Label에 text 추가
    Label의 속성 변경(글자 색, textAlign)
    view의 배경 색 변경

--- KakaoProfile/KakaoProfile/ViewController.swift
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.lightGray
+        self.nameLabel.text = "David"
+        self.nameLabel.textAlignment = NSTextAlignment.center
+        self.nameLabel.textColor = UIColor.white
+        self.nameLabel.backgroundColor = UIColor.clear
+        self.descriptionLabel.text = "행복 멀리 없다!"
+        self.descriptionLabel.textAlignment = NSTextAlignment.center
+        self.descriptionLabel.textColor = UIColor.secondarySystemBackground
     }
 
 
