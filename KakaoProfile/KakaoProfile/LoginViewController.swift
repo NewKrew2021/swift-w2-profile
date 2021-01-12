15bc1822be5c65cd5465863eea46f4517ad2a3e9
eden.joon <eden.joon@kakaocorp.com>
2021-01-12 17:43:34 +0900

    Implement UIImagePickerController
    이미지 컨트롤러의 타입을 정하고, 사용가능 여부를 확인안 후
    시스템 뷰 컨트롤러를 띄울 수 있도록 했다.
    사진 선택한 것을 가져오기 위해 deleagate를 공부할 예정

--- KakaoProfile/KakaoProfile/LoginViewController.swift
+++ KakaoProfile/KakaoProfile/LoginViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
 
+class LoginViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    weak open var delegate: (UIImagePickerControllerDelegate & UINavigationControllerDelegate)?
+    
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var changeButton: UIButton!
-  
+    
     var nameText: String?
     var descriptionText: String?
+    let imagePickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +30,22 @@ class LoginViewController: UIViewController {
         changeButton.layer.borderWidth = 1
         changeButton.layer.borderColor = UIColor.clear.cgColor
         changeButton.clipsToBounds = true
+        
+        imagePickerController.delegate = self//imagePickerController에서 발생하는 일을 내가 처리하겠다(위임)
     }
     
     @IBAction func closeButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func selectImageButtonTouched(_ sender: Any) {
+        let type = UIImagePickerController.SourceType.photoLibrary
+        guard UIImagePickerController.isSourceTypeAvailable(type) else {return}
+        imagePickerController.sourceType = type
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("didFinishPickingMediaWithInfo", info)
     }
 }
