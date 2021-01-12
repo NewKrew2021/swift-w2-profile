ff7b133bf9c4d7119535a98ccef342652f8567c8
eden.joon <eden.joon@kakaocorp.com>
2021-01-12 17:56:31 +0900

    실행 순서에 맞게 함수 위치 수정
    뷰 컨트롤러의 함수들을 실행 순서에 맞게 위치를 수정했다.

--- KakaoProfile/KakaoProfile/ViewController.swift
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -24,12 +24,12 @@ class ViewController: UIViewController {
         self.descriptionLabel.text = "팬 소음이 너무 크다"
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = segue.destination as? LoginViewController {
-            destination.nameText = self.nameLabel.text ?? ""
-            destination.descriptionText = self.descriptionLabel.text ?? ""
-            print("segue")
-        }
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
     }
     
     @IBAction func editButtonTouched(_ sender: Any) {
@@ -37,15 +37,13 @@ class ViewController: UIViewController {
         self.nameLabel.backgroundColor = UIColor.yellow
         self.nameLabel.alpha = 0.5
         self.descriptionLabel.text = "크루미션"
-        print("ibaction")
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        print(#file, #line, #function, #column)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        print(#file, #line, #function, #column)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? LoginViewController {
+            destination.nameText = self.nameLabel.text ?? ""
+            destination.descriptionText = self.descriptionLabel.text ?? ""
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -56,4 +54,3 @@ class ViewController: UIViewController {
         print(#file, #line, #function, #column)
     }
 }
-
