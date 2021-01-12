2977f6ee70627b5afb9068189f747d1c2c84c7cf
David Window <david.window@kakaocorp.com>
2021-01-12 13:37:10 +0900

    IBAction 프로그래밍 요구사항 충족
    - 버튼을 ViewController에 IBAction으로 연결
    - 추가된 메소드 내용 구현

--- KakaoProfile/KakaoProfile/ViewController.swift
+++ KakaoProfile/KakaoProfile/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
+    @IBAction func editButtonTouched(_ sender: Any) {
+        self.nameLabel.textColor = UIColor.blue
+        self.nameLabel.backgroundColor = UIColor.yellow
+        self.nameLabel.alpha = 0.5
+        self.descriptionLabel.text = "크루미션"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- README.md
+++ README.md
@@ -12,3 +12,17 @@ david의 README
 
 ![image-20210111175805578](/Users/ihochang/Library/Application Support/typora-user-images/image-20210111175805578.png)
 
+
+
+### Second Commit - 0112 12:30
+
+- InageView, Label, Button 추가
+- ViewController에 IBOutlet으로 연결
+- Label Text 추가, Label Attribute 변경
+
+
+
+### Third Commit - 0112 13:30
+
+- Button을 ViewController에 IBAction으로 연결
+- 추가한 IBAction에 몇가지 속성 변경 코드 추가
