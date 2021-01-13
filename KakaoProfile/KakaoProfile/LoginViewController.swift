01675950b66f496fab065fb12568d806808e4ba6
David Window <david.window@kakaocorp.com>
2021-01-13 12:01:08 +0900

    add profileChangeButton
    - add Button
    - set some attributes of button

--- KakaoProfile/KakaoProfile/LoginViewController.swift
+++ KakaoProfile/KakaoProfile/LoginViewController.swift
@@ -13,6 +13,8 @@ class LoginViewController: UIViewController {
     var descriptionText : String = ""
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileChangeButton: UIButton!
     @IBOutlet weak var profileLabel: UILabel!
     @IBOutlet weak var nameLabelEdited: UITextField!
     @IBOutlet weak var descriptionLabelEdited: UITextField!
@@ -20,6 +22,9 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red: 218/255, green: 217/255, blue: 255/255, alpha: 1)
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.width * 0.45
+        self.profileChangeButton.backgroundColor = .white
+        self.profileChangeButton.layer.cornerRadius = self.profileChangeButton.layer.frame.width/2
         self.profileLabel.text = "프로필"
         self.profileLabel.textColor = .white
         self.cancelButton.tintColor = .white
