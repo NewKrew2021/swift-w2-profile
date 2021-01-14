7374b28833e7597fe6820a7e7e02e32921db33aa
blue.1 <blue.1@kakaocorp.com>
2021-01-14 11:42:23 +0900

    로그인화면 imageView, textField, Button 추가

---
+++ KakaoProfile/KakaoProfile/ViewController/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  KakaoProfile
+//
+//  Created by 이청원 on 2021/01/14.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
