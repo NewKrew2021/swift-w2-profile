fa54ee56742ef1a8a51da32f6ed97fde788ecc13
blue.1 <blue.1@kakaocorp.com>
2021-01-14 14:04:50 +0900

    로그인 기능 구현

--- KakaoProfile/KakaoProfile/DB/UserInfo.swift
+++ KakaoProfile/KakaoProfile/DB/UserInfo.swift
@@ -9,8 +9,23 @@ import Foundation
 
 class UserInfo {
     
-    enum CheckState: Int {
-        case success = 0, noID, wrongPw
+    enum CheckState: Int, CustomStringConvertible {
+        case success = 0, blankID, blankPw, noID, wrongPw
+        
+        var description: String {
+            switch self {
+            case .success:
+                return "로그인에 성공하였습니다"
+            case .blankID:
+                return "아이디를 입력해주세요"
+            case .blankPw:
+                return "비밀번호를 입력해주세요"
+            case .noID:
+                return "아이디가 존재하지 않습니다"
+            case .wrongPw:
+                return "비밀번호가 일치하지 않습니다"
+            }
+        }
     }
     
     struct User {
@@ -25,6 +40,11 @@ class UserInfo {
     ]
     
     func checkUser(id:String, pw:String) -> CheckState {
+        if id == "" {
+            return .blankID
+        } else if pw == "" {
+            return .blankPw
+        }
         for user in model {
             if user.userId == id {
                 if user.userPassword == pw {
--- KakaoProfile/KakaoProfile/ViewController/LoginViewController.swift
+++ KakaoProfile/KakaoProfile/ViewController/LoginViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var id: UITextField!
+    @IBOutlet var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let idPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        let passwordPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        self.id.leftView = idPaddingView
+        self.password.leftView = passwordPaddingView
+        self.id.leftViewMode = .always
+        self.password.leftViewMode = .always
+        
     }
     
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        let userInfo = UserInfo()
+        let checkUser = userInfo.checkUser(id: id.text ?? "", pw: password.text ?? "")
+        switch checkUser {
+        case .success:
+            showToast(controller: self, message: "\(UserInfo.CheckState.success)", seconds: 0.5)
+        case .blankID:
+            showToast(controller: self, message: "\(UserInfo.CheckState.blankID)", seconds: 0.5)
+        case .blankPw:
+            showToast(controller: self, message: "\(UserInfo.CheckState.blankPw)", seconds: 0.5)
+        case .noID:
+            showToast(controller: self, message: "\(UserInfo.CheckState.noID)", seconds: 0.5)
+        case .wrongPw:
+            showToast(controller: self, message: "\(UserInfo.CheckState.wrongPw)", seconds: 0.5)
+        default:
+            print("")
+        }
+    }
 }
